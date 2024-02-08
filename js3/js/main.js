2f5180b6fb9dc1c45b464db7b07b36d93ec8e463
JosephGreenburg <joegreenburg@peakroi.com>
2024-02-08 08:54:07 -0600

    Add all files to repository

---
+++ js3/js/main.js
@@ -0,0 +1,7 @@
+//Strings
+const myVariable = "Mathematics";
+
+//The length property
+console.log("Every Good boy spells well".split(" "));
+
+//Dot Notation master
---
+++ js4/js/main.js
@@ -0,0 +1,24 @@
+//Numbers
+const myNumber = 42;
+//A number with a decimal is a float which references the "floating point"
+const myFloat = 42.0151
+//string number
+const myString = "42.123abc";
+// console.log();
+// console.log(Number(false));
+
+
+//Number Methods
+//The Number.isInteger() method determines whether the passed value is an integer.
+
+//The Number.parseFloat() method parses an argument and returns a floating point number.
+// If a number cannot be parsed from the argument, it returns NaN
+
+//The toFixed() method formats a number accoding to how many decimal points you provide as the parameter.
+
+//Number.isInteger() method determines whether the passed value is an integer.
+
+//The Number.parseInt() method parses a string argument and returns a integer.
+
+
+console.log(Number.parseFloat("4.237abc").toFixed(2).toString());
