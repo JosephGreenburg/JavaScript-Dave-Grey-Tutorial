0f160fdcc4eb17fe049b325d8741210cb48dabcf
JosephGreenburg <joegreenburg@peakroi.com>
2024-02-28 09:17:49 -0600

    Updated the js7 exercise. Completed.

--- js7/assets/js/main.js
+++ js7/assets/js/main.js
@@ -1,13 +1,30 @@
 // Conditionals: If Statements
 
-// Set variable soup
-let soup;
 
-// Set variable reply;
+//Syntax:
+
+// Set customer is banned - if true all of other statements in the block will not be executed
+let customerIsBanned = false;
+
+// Set crackers as another argument to check
+let crackers = false;
+// Set variable soup - else if the soup exists then it will give the customer their soup.
+let soup = "Chicken Noodle Soup";
+
+// Set variable reply - else if a message will be set to the variable. 
 let reply;
 
-if (soup) {
+
+if(customerIsBanned) {
+    reply = "No soup for you!"; 
+} else if(soup && crackers) {
+    // Temperate literal string
+    reply = `Here's your order of ${soup} and crackers.`;
+
+} else if (soup) {
+    // Temperate literal string
     reply = `Here's your order of ${soup}`;
+
 } else {
     reply = "Sorry, we're out of soup.";
 }
