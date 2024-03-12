78da41296f498374409ed285a4aa2c49331043e0
JosephGreenburg <joegreenburg@peakroi.com>
2024-03-12 09:21:02 -0500

    Added notes to User Input methods

--- js10/main.js
+++ js10/main.js
@@ -1,18 +1,27 @@
-//User input
+/**
+ * User input methods
+ * 
+ * - alert()
+ * - confirm()
+ * - prompt()
+ */
 
-// alert("User Input");
+/**
+ * alert("User Input");
+ */
 //let alertWindow = alert("Hello World");
 
-//Confirm - Different than alert(). The console will use the value true/false depending
-//on which button the user clicks on the pop-up.
-
+/**
+ * Confirm - Different than alert(). The console will use the value true/false depending
+ * on which button the user clicks on the pop-up.
+ */
 // let myBoolean = confirm("Okay === True\nCancel === False");
 // console.log(myBoolean);
 
-//Prompt is similar to alert() and confirm(), but whatever the user types in
-//in will be kept data.
-
-// let name = prompt("Please enter your name: ");
+/**
+ * Prompt is similar to alert() and confirm(), but whatever the user types in will be kept data.
+ */
+let name = prompt("Please enter your name: ");
 
 if (name) {
     console.log(name)
