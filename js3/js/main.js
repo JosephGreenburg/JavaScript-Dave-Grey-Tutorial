eae8d7591f5f9aa9b308a3ed3c4ba62f3b9dd6c7
JosephGreenburg <joegreenburg@peakroi.com>
2024-03-12 08:34:11 -0500

    Added placeholder for js26. Updated js3 with notes and an additional method.

--- js3/js/main.js
+++ js3/js/main.js
@@ -1,7 +1,16 @@
-//Strings
+/**
+ * Strings
+ */
 const myVariable = "Mathematics";
 
-//The length property
-console.log("Every Good boy spells well".split(" "));
+/**
+ * .length method - Shows the length of the variable. 
+ */
+console.log(myVariable.length);//Returns 11
+
+/**
+ * .split() method - separates the string into elements in an array, based on the character that should be in between them. In this case space or " "
+ */
+console.log("Every Good boy spells well".split(" "));//Returns an array
 
 //Dot Notation master
